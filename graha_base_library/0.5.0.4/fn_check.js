ec15da8d3e4a3d8d62d140f5720994e3ba737565
logicielkr <graha@graha.kr>
2022-11-11 14:02:21 +0900

    1. Graha Base Library 의 버전을 0.5.0.4 로 변경
    - input.width.css 변경
    - chrome 에서 "width: -webkit-fill-available;" 가 있으면,
    - input 의 width 를 지정하지 않은 것과 같다.

---
+++ graha_base_library/0.5.0.4/fn_check.js
@@ -0,0 +1,118 @@
+function _minValue(form, name, value) {
+	if(!_notNull(form, name)) {
+		return true;
+	}
+	var obj = form[name];
+	var v = obj.value;
+	v = v.replace(/,/g, "");
+	v = v.replace(/ /g, "");
+	if(v == "") {
+		return false;
+	}
+	if(v.indexOf(".") >= 0 || value.toString().indexOf(".") >= 0) {
+		if(parseFloat(v) >= value) {
+			return true;
+		}
+	} else {
+		if(parseInt(v) >= value) {
+			return true;
+		}
+	}
+	return false;
+}
+function _maxValue(form, name, value) {
+	if(!_notNull(form, name)) {
+		return true;
+	}
+	var obj = form[name];
+	var v = obj.value;
+	v = v.replace(/,/g, "");
+	v = v.replace(/ /g, "");
+	if(v == "") {
+		return true;
+	}
+	if(v.indexOf(".") >= 0 || value.toString().indexOf(".") >= 0) {
+		if(parseFloat(v) <= value) {
+			return true;
+		}
+	} else {
+		if(parseInt(v) <= value) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function _minLength(form, name, len) {
+	if(!_notNull(form, name)) {
+		return true;
+	}
+	var obj = form[name];
+	if((obj.value).length >= len) {
+		return true;
+	}
+	return false;
+}
+function _maxLength(form, name, len) {
+	if(!_notNull(form, name)) {
+		return true;
+	}
+	var obj = form[name];
+	if((obj.value).length <= len) {
+		return true;
+	}
+	return false;
+}
+function _notNull(form, name) {
+	var obj = form[name];
+	if(obj.value == null) {
+		return false;
+	} else if(obj.value == "") {
+		return false;
+	}
+	return true;
+}
+function _numberFormat(form, name, t) {
+	if(!_notNull(form, name)) {
+		return true;
+	}
+	var obj = form[name];
+	var v = obj.value;
+	v = v.replace(/,/g, "");
+	v = v.replace(/ /g, "");
+	if(v == "") {
+		return true;
+	}
+	if(t == "int" || t == "long") {
+		if(!isNaN(Number(v)) && !isNaN(parseInt(v)) && parseInt(v).toString() == v) {
+			if(t == "int") {
+				if(parseInt(v) >= -2147483648 && parseInt(v) <= 2147483647) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+	} else if(t == "float" || t == "double") {
+		if(!isNaN(Number(v)) && !isNaN(parseFloat(v))) {
+			if(t == "float") {
+				if(parseFloat(v) >= 1.4E-45 && parseFloat(v) <= 3.4028235E38) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+	return false;
+}
+function _format(form, name, t) {
+	if(!_notNull(form, name)) {
+		return true;
+	}
+	var obj = form[name];
+	var v = obj.value;
+	return (new RegExp(t)).test(v);
+}
